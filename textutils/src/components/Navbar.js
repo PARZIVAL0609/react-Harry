328056eca206797ce488ca26d4112bf0f2ba9aee
PARZIVAL0609 <dhruvshah6903@gmail.com>
2024-05-08 21:09:27 +0530

    Dark mode

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -32,7 +32,7 @@ export default function Navbar(props) {
               </a>
             </li>
           </ul>
-          <form className="d-flex" role="search">
+          {/* <form className="d-flex" role="search">
             <input
               className="form-control me-2"
               type="search"
@@ -42,7 +42,7 @@ export default function Navbar(props) {
             <button className="btn btn-primary" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
         </div>
       </div>
     </nav>
@@ -55,6 +55,6 @@ Navbar.propTypes = {
 };
 
 Navbar.defaultProps = {
-    title: 'Set title',
-    aboutText: 'About Here' 
+  title: "Set title",
+  aboutText: "About Here",
 };
