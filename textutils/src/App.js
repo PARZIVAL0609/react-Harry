f36f34f40ed6c2c0f0f89bb9631c531f61e99213
PARZIVAL0609 <dhruvshah6903@gmail.com>
2024-05-09 23:06:34 +0530

    dark mode ultra pro max

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 // import About from "./components/About";
 
 function App() {
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'rgb(29 30 67)';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+  const [mode, setMode] = useState('light');
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode = {mode} toggleMode = {toggleMode}/>
       <div className="container">
-        <TextForm heading="Enter the text to analyse below"/>
+        <TextForm heading="Enter the text to analyse below" mode = {mode}/>
         {/* <About /> */}
       </div>
     </>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -10,11 +10,11 @@ export default function TextForm(props) {
     setText(newText);
   };
   const handleClearClick = () => {
-    let newText = '';
+    let newText = "";
     setText(newText);
   };
   const handleCopy = () => {
-    let text = document.getElementById('myBox');
+    let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
   };
@@ -26,20 +26,26 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
-            placeholder="Enter text here"
             className="form-control"
             id="myBox"
             rows="8"
             value={text}
             onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "rgb(29 30 67)",
+              color: props.mode === "light" ? "black" : "white"
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>
@@ -58,12 +64,17 @@ export default function TextForm(props) {
           Remove Extra Spaces
         </button>
       </div>
-      <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
         <h2>Your text summary</h2>
-        <p>{text.split(' ').length} words, {text.length} characters</p>
-        <p>{0.008 * text.split(' ').length} minutes to read</p>
+        <p>
+          {text.split(" ").length} words, {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter something..."}</p>
       </div>
     </>
   );
