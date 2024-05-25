7b846a317d47df4ff0ec383a20f9c8d9e04b08b3
Dhruv Shah <dhruvshah6903@gmail.com>
2024-05-25 17:20:28 +0530

    Gihub pages hosting

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import About from "./components/About";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import About from "./components/About";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -36,31 +36,27 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Navbar
-          title="TextUtils"
-          mode={mode}
-          toggleMode={toggleMode}
-          aboutText="About"
-        />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />}></Route>
-            <Route
+      {/* <Router> */}
+      <Navbar
+        title="TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+        aboutText="About"
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About />}></Route> */}
+        {/* <Route
               exact
               path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter Text Here"
-                  mode={mode}
-                />
-              }
+              element={ */}
+        <TextForm showAlert={showAlert} heading="Enter Text Here" mode={mode} />
+        {/* }
             ></Route>
-          </Routes>
-        </div>
-      </Router>
+          </Routes> */}
+      </div>
+      {/* </Router> */}
     </>
   );
 }
