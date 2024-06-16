294dd6421c49ee5f34f4b8f3046a53d55a6709bd
Dhruv Shah <dhruvshah6903@gmail.com>
2024-06-17 00:04:17 +0530

    Mugen Scroll

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
     country: "in",
     pageSize: 9,
     category: "general",
+    totalResults: 0,
   };
 
   static propTypes = {
@@ -16,9 +18,9 @@ export class News extends Component {
     category: PropTypes.string,
   };
 
-  capitalize = (word) =>{
+  capitalize = (word) => {
     return word[0].toUpperCase() + word.slice(1);
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -30,8 +32,8 @@ export class News extends Component {
     document.title = `NewsMonkey - ${this.capitalize(this.props.category)}`;
   }
 
-  async updateNews(){
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=878cfe49e1e8417eac579d89824049e6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=637878ef9cd34b319e0a9d5f72162e3d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -47,48 +49,77 @@ export class News extends Component {
     this.updateNews();
   }
 
-  handlePrevClick = async () => {
-    this.setState({page: this.state.page - 1});
-    this.updateNews();
-  };
+  // handlePrevClick = async () => {
+  //   this.setState({ page: this.state.page - 1 });
+  //   this.updateNews();
+  // };
 
-  handleNextClick = async () => {
-    this.setState({page: this.state.page + 1});
-    this.updateNews();
+  // handleNextClick = async () => {
+  //   this.setState({ page: this.state.page + 1 });
+  //   this.updateNews();
+  // };
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=637878ef9cd34b319e0a9d5f72162e3d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
   };
 
   render() {
     return (
-      <div className="container my-3">
-        <h1 className="text-center">NewsMonkey - Top {this.capitalize(this.props.category)} Headlines</h1>
+      <>
+        <h1 className="text-center">
+          NewsMonkey - Top {this.capitalize(this.props.category)} Headlines
+        </h1>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88) + "..."
-                        : ""
-                    }
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
-                    }
-                    newsUrl={element.url ? element.url : ""}
-                    author={element.author ? element.author: 'Unknown'}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-between">
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={<Spinner />}
+          
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element, index) => {
+                return (
+                  <div
+                    className="col-md-4"
+                    key={element.url ? element.url + index : index}
+                  >
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88) + "..."
+                          : ""
+                      }
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
+                      }
+                      newsUrl={element.url ? element.url : ""}
+                      author={element.author ? element.author : "Unknown"}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+
+        {/* <div className="container d-flex justify-content-between">
           <button
             type="button"
             className="btn btn-dark"
@@ -116,8 +147,8 @@ export class News extends Component {
           >
             Next &rarr;
           </button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
