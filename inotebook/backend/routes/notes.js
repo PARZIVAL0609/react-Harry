ef7984ad281eaebc6ad2f4e388641fa2c244ce61
Dhruv Shah <dhruvshah6903@gmail.com>
2024-06-20 23:36:00 +0530

    update note endpoint

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -50,4 +50,37 @@ router.post(
   }
 );
 
+//ROUTE 3: Update a note using: PUT "/api/notes/updatenote". Login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  //Create newNote object
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  //Find the note to be updated
+  let note = await Note.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found");
+  }
+
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed");
+  }
+
+  note = await Note.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.json({ note });
+});
+
 module.exports = router;
