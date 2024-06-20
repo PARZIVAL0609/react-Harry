d29d05b4d280daba52623db9b4d9bb58d9e5c0af
Dhruv Shah <dhruvshah6903@gmail.com>
2024-06-20 20:39:04 +0530

    login endpoint

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "ochagaume";
 
@@ -50,10 +50,55 @@ router.post(
       };
       const authToken = jwt.sign(data, JWT_SECRET);
 
-      res.json({authToken});
+      res.json({ authToken });
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("Some Error occured");
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//Authenticate a user using: POST "/api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // If there are errors, return bad requests and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please enter valid credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please enter valid credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(payload, JWT_SECRET);
+
+      res.json({ authToken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
     }
   }
 );
