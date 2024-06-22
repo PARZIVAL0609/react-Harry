323fb52e4b7e2be11983185d1dd6205ddddf5ea7
Dhruv Shah <dhruvshah6903@gmail.com>
2024-06-22 21:03:41 +0530

    useLocation for active class in navbar

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,17 +1,7 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/Notes/noteContext'
+import React from "react";
 
 const About = () => {
-    const a = useContext(noteContext)
-    useEffect(()=>{
-        a.update();
-        // eslint-disable-next-line
-    },[])
-  return (
-    <div>
-      This is About {a.state.name} of class {a.state.class}
-    </div>
-  )
-}
+  return <div>This is About</div>;
+};
 
-export default About
+export default About;
--- inotebook/src/context/Notes/NoteState.js
+++ inotebook/src/context/Notes/NoteState.js
@@ -1,25 +1,8 @@
-import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Dhruv",
-    class: "10F",
-  };
-
-  const [state, setState] = useState(s1);
-
-  const update = ()=>{
-    setTimeout(() => {
-        setState({
-            name: "Parzival",
-            class: "A",
-        })
-    }, 1000);
-  }
-
   return (
-    <noteContext.Provider value={{state, update}}>{props.children}</noteContext.Provider>
+    <noteContext.Provider value={{}}>{props.children}</noteContext.Provider>
   );
 };
 
