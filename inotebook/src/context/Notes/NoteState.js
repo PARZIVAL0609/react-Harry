35949dc4fce9128e701fc9d37a895772c325ef38
Dhruv Shah <dhruvshah6903@gmail.com>
2024-06-23 18:55:26 +0530

    addnote functionality

--- inotebook/src/context/Notes/NoteState.js
+++ inotebook/src/context/Notes/NoteState.js
@@ -79,8 +79,32 @@ const NoteState = (props) => {
 
   const [notes, setNotes] = useState(notesInitial);
 
+  //Add a note
+  const addNote = (title, description, tag) => {
+    //todo api
+    console.log("Adding")
+    const note = {
+      _id: "3676f2b3f464093fb3d53953",
+      user: "66743a0a2320b67f391d9aa7",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2024-06-22T15:50:12.353Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+
+  //Delete a note
+  const deleteNote = (id) => {};
+
+  //Edit a note
+  const editNote = (id) => {};
+
   return (
-    <noteContext.Provider value={{ notes, setNotes }}>
+    <noteContext.Provider
+      value={{ notes, setNotes, addNote, deleteNote, editNote }}
+    >
       {props.children}
     </noteContext.Provider>
   );
